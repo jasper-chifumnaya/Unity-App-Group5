8bc7bd473f12bd83c0a276db3196f9f9481d66bd
anthony_okocha-ojeah <anthony.okocha-ojeah@pau.edu.ng>
2026-05-21 17:47:00 +0100

    Added a function which allows new users to create an account which is saved on users.csv

--- auth.py
+++ auth.py
@@ -12,3 +12,17 @@ class LoginScreen(tk.Frame):
             for r in csv.reader(f):
                 if r[0] == self.u.get() and r[1] == self.p.get():
                     Self.master.master.master.show("Aid")
+
+
+#Anthony wrote this function which allows new users to create an account which is saved on users.csv
+
+class JoinScreen(tk.Frame):
+    def __init__(self, parent, ctrl):
+        super().__init__(parent, bg="#F7F7F7")
+        self.u = tk.Entry(self); self.u.pack()
+        self.p = tk.Entry(self, show="*"); self.p.pack()
+        tk.Button(self, text="REGISTER", bg="#490D52", fg="white", command=self.save).pack()
+    def save(self):
+        with open('users.csv', 'a', newline='') as f:
+            csv.writer(f).writerow([self.u.get(), self.p.get()])
+            self.master.master.master.show("Login")
